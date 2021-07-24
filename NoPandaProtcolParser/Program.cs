aa30bf17681003bdb9deae84cebab136d8909c76
Paul Schönau <paul@schoenaunet.de>
2021-07-25 00:28:53 +0200

    Initial commit

---
+++ NoPandaProtcolParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NoPandaProtcolParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProtocolInfo info = ProtocolInfo.Parse("myapp://connect/1281238");
+
+            Console.WriteLine($"Name: {info.ProtocolName}; Args: {string.Join(" | ", info.ProtocolArgs)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NoPandaProtcolParser/ProtocolInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoPandaProtcolParser
+{
+    public class ProtocolInfo
+    {
+        public ProtocolInfo(string protocolName, string[] protocolArgs)
+        {
+            ProtocolName = protocolName;
+            ProtocolArgs = protocolArgs;
+        }
+
+        // myapp://connect/8bkxa221a
+        public string ProtocolName { get; set; }
+        public string[] ProtocolArgs { get; set; }
+
+        /// <summary>
+        /// Parse a protocol call to a protocol info
+        /// </summary>
+        /// <param name="n">The protocol call</param>
+        /// <returns></returns>
+        public static ProtocolInfo Parse(string n)
+        {
+            string name = n.Split(":")[0];
+            string rawArgs = n.Substring(name.Length + 3);
+            string[] args = rawArgs.Split("/");
+            return new ProtocolInfo(name, args);
+        }
+    }
+}
